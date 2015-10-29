25c2c405c91343f1d5ea8027dd912fbac3c3b014
Tom Pelsmaeker <tlpelsmaeker@gmail.com>
2015-10-29 12:28:32 +0100

    question2

--- q2.java
+++ q2.java
@@ -3,18 +3,21 @@ class Person {
    // Properties of the class...
    private String name;
    private int    age;
+   private int hands;
+   
     
    // Constructor of the class...
-   public Person(String aName, int anAge) {
+   public Person(String aName, int anAge, int noHands) {
       name = aName;
       age  = anAge;
+      hands = noHands;
    }
 
    // Methods of the class...
    public void talk() {
       System.out.println("Hi, my name's " + name);
       System.out.println("and my age is " + age);
-      System.out.println();
+      System.out.println("and I have " + hands + " hands");
    }
    public void commentAboutAge() {
       if (age < 5) {
@@ -23,6 +26,17 @@ class Person {
       if (age == 5) {
          System.out.println("time to start school");
       }
+	  if (age > 60) {
+         System.out.println("old person");
+      }
+   }
+
+   public void growOlder() {
+      age++;
+   }
+
+   public void giveKnightnood() {
+      name = "sir " + name;
    }
 
 }
@@ -32,11 +46,16 @@ class PersonTest {
    // The main method is the point of entry into the program...
    public static void main(String[] args) {
 
-      Person ls = new Person("Luke Skywalker",34);
-      Person wp = new Person("Winston Peters",48);
+      Person ls = new Person("Luke Skywalker",34,1);
+      Person wp = new Person("Winston Peters",48,2);
 
       ls.talk();
       wp.talk();
+      
+      ls.commentAboutAge();
+      wp.commentAboutAge();
 
+      wp.growOlder();
+	  wp.giveKnightnood();
    }
 }
