1e369f13fdcc66e42f7e4e254dfbc425af6d4801
Tom Pelsmaeker <tlpelsmaeker@gmail.com>
2015-10-29 14:36:20 +0100

    q7 final

---
+++ q7.java
@@ -0,0 +1,59 @@
+class Iffy {
+   private int anIntProp = 42;
+   public static void main(String[] args) {
+      Iffy me = new Iffy();
+      me.callSomeMethods();
+   }
+
+   public void callSomeMethods() {
+      // add statements here to exercise the other methods ...
+      warnIfNegative(42);
+      warnIfNegative(0);
+      warnIfNegative(-42);
+      warnIfNegative(-6542);
+      resetIfNegative();
+      isInRange(0, 42, 68);
+   }
+
+   /**
+    * Print out a warning if the value is negative
+    * otherwise don't do anything.
+    */
+   public void warnIfNegative(int theValue) {
+      if(theValue < 0){
+         System.out.println("Caution - negative value given (" + theValue + ")");
+      }   
+   }
+  
+   /**
+    * Print out a warning if the value of anIntProp is negative
+    * and also set the value to zero.
+    * Otherwise don't do anything.
+    */
+   public void resetIfNegative() {
+      if(anIntProp < 0) {
+         System.out.println("Caution - negative value given (" + anIntProp + ")");
+         anIntProp = 0;
+      }
+   }
+
+   /**
+    * return true if value is between upperBound and LowerBound
+    * (or equal to either bound) otherwise return false.
+    */
+   public boolean isInRange(int value, int upperBound, int lowerBound) {
+     if(lowerBound <= value && value <= upperBound ){
+        return true;
+     }
+     else {
+        return false;
+     }
+   }
+
+   public boolean isInRangeIfLess(int value, int upperBound, int lowerBound) {
+   //  what goes here?
+      return lowerBound <= value && value <= upperBound;
+   }
+}
+
+
